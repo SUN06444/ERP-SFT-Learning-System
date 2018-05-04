243b5165a8096aa7d14a312d6f9d65081ff5e269
SUN06444 <msc06444@gmail.com>
2018-05-05 04:11:19 +0800

    no message

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://ncutim-learning-system.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
